dc50cf50792aee220d02be9a12e00d9962da0147
Mateus Prado <maateussp@gmail.com>
2026-02-03 12:01:00 -0300

    guard

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { AuthTokenGuard } from 'src/auth/guard/auth.token.guard';
 
+@UseGuards(AuthTokenGuard)
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
